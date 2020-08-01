a56542c89e37791534259ef4cf363f59f8d0d803
jlargot <jlargo@fieraservices.com>
2020-08-01 11:33:16 -0500

    Adding Integration tests

--- Java/api/src/test/java/com/fiera/api/Controller/UserControllerIT.java
+++ Java/api/src/test/java/com/fiera/api/Controller/UserControllerIT.java
@@ -1,47 +1,53 @@
 package com.fiera.api.Controller;
 
-import java.util.Arrays;
-import java.util.List;
-
-import com.fiera.api.controllers.UserController;
 import com.fiera.api.dtos.UserDTO;
-import com.fiera.api.services.UserService;
+import com.fiera.api.exceptions.CustomBadRequestException;
+import com.fiera.api.exceptions.CustomNotFoundException;
 
 import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.client.RestTemplate;
 
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.assertj.core.api.BDDAssertions.then;
 
-@ExtendWith(MockitoExtension.class)
-@WebMvcTest(UserController.class)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestDatabase
 public class UserControllerIT {
 
-    @Autowired
-    private MockMvc mvc;
+    @LocalServerPort
+    private int port;
 
-    @MockBean
-    private UserService userService;
+    private TestRestTemplate restTemplate = new TestRestTemplate();
 
-    @Test
-    public void shouldReturnAllUsers() throws Exception {
+    private UserDTO userDTO;
 
-        UserDTO userDTO = new UserDTO();
+    private static HttpHeaders headers = new HttpHeaders();
+
+    @BeforeAll
+    public static void init() throws Exception
+    {
+        String accessToken = obtainAccessToken();
+        headers.setBearerAuth(accessToken);
+    }
+
+    @BeforeEach
+    public void setup() {
+        userDTO = new UserDTO();
         userDTO.setUserId(1L);
         userDTO.setDocNumber("123456789");
         userDTO.setFirstName("Luke");
@@ -49,24 +55,158 @@ public class UserControllerIT {
         userDTO.setEmail("jedi@email.com");
         userDTO.setPhone("987654321");
         userDTO.setAddress("123 death star avenue");
-
-        List<UserDTO> users = Arrays.asList(userDTO);
-
-        given(userService.getUsers()).willReturn(users);
-
-        String accessToken = obtainAccessToken();
-
-        mvc.perform(
-                get("/users")
-                .header("Authorization", "Bearer " + accessToken)
-                .contentType(MediaType.APPLICATION_JSON))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$").isNotEmpty())
-                .andExpect(jsonPath("$[0].firstName").value(userDTO.getFirstName()));
     }
 
-    private String obtainAccessToken() throws Exception {
+    @Test
+    public void shouldReturnAllUsers() {
+
+        HttpEntity<String> request = new HttpEntity<String>(null, headers);
+
+        ResponseEntity<UserDTO[]> actual = restTemplate.exchange(createURLWithPort("/users"), HttpMethod.GET, request,
+                UserDTO[].class);
+
+        then(actual).as("Check if we have a response").isNotNull();
+        then(actual.getStatusCodeValue()).as("Check if we have the expected status").isEqualTo(200);
+        then(actual.getBody()).as("Check if we have a result").isNotNull();
+    }
+
+    @Test
+    void shouldReturnUserById() throws CustomNotFoundException {
+
+        HttpEntity<String> request = new HttpEntity<String>(null, headers);
+
+        ResponseEntity<UserDTO> actual = restTemplate.exchange(createURLWithPort("/users/1"), HttpMethod.GET, request,
+        UserDTO.class);
+
+        then(actual).as("Check if we have a response").isNotNull();
+        then(actual.getStatusCodeValue()).as("Check if we have the expected status").isEqualTo(200);
+        then(actual.getBody()).as("Check if we have a result").isNotNull();
+    }
+
+    @Test
+    void badIdShouldReturnNotFound() throws CustomNotFoundException {
+    
+        HttpEntity<String> request = new HttpEntity<String>(null, headers);
+
+        ResponseEntity<UserDTO> actual = restTemplate.exchange(createURLWithPort("/users/2"), HttpMethod.GET, request,
+        UserDTO.class);
+
+        then(actual).as("Check if we have a response").isNotNull();
+        then(actual.getStatusCodeValue()).as("Check if we have the expected status").isEqualTo(404);
+    }
+
+    @Test
+    void badIdShouldReturnBadRequest() throws CustomNotFoundException {
+    
+        HttpEntity<String> request = new HttpEntity<String>(null, headers);
+
+        ResponseEntity<UserDTO> actual = restTemplate.exchange(createURLWithPort("/users/a"), HttpMethod.GET, request,
+        UserDTO.class);
+
+        then(actual).as("Check if we have a response").isNotNull();
+        then(actual.getStatusCodeValue()).as("Check if we have the expected status").isEqualTo(400);        
+    }
+
+    @Test
+    void shouldSaveUserSuccessFully() throws CustomBadRequestException {
+
+        userDTO.setUserId(null);
+
+        HttpEntity<UserDTO> request = new HttpEntity<UserDTO>(userDTO, headers);
+
+        ResponseEntity<Object> actual = restTemplate.exchange(createURLWithPort("/users"), HttpMethod.POST, request,
+        Object.class);
+    
+        then(actual).as("Check if we have a response").isNotNull();
+        then(actual.getStatusCodeValue()).as("Check if we have the expected status").isEqualTo(201);
+        then(actual.getHeaders().get("location").get(0)).as("Check if we have the expected location at the header").contains(createURLWithPort("/users/"));
+    }
+
+    @Test
+    void badRequestShouldReturnException() throws CustomBadRequestException {
+
+        HttpEntity<UserDTO> request = new HttpEntity<UserDTO>(userDTO, headers);
+
+        ResponseEntity<Object> actual = restTemplate.exchange(createURLWithPort("/users"), HttpMethod.POST, request,
+        Object.class);
+
+        then(actual).as("Check if we have a response").isNotNull();
+        then(actual.getStatusCodeValue()).as("Check if we have the expected status").isEqualTo(400);
+    }
+
+    @Test
+    void shouldUpdateUserSuccessFully() throws CustomBadRequestException, CustomNotFoundException {
+
+        HttpEntity<UserDTO> request = new HttpEntity<UserDTO>(userDTO, headers);
+
+        ResponseEntity<Object> actual = restTemplate.exchange(createURLWithPort("/users/1"), HttpMethod.PUT, request,
+        Object.class);
+
+        then(actual).as("Check if we have a response").isNotNull();
+        then(actual.getStatusCodeValue()).as("Check if we have the expected status").isEqualTo(200);
+    }
+
+    @Test
+    void badIdsShouldReturnException() throws CustomBadRequestException, CustomNotFoundException {
+
+        HttpEntity<UserDTO> request = new HttpEntity<UserDTO>(userDTO, headers);
+
+        ResponseEntity<Object> actual = restTemplate.exchange(createURLWithPort("/users/2"), HttpMethod.PUT, request,
+        Object.class);
+
+        then(actual).as("Check if we have a response").isNotNull();
+        then(actual.getStatusCodeValue()).as("Check if we have the expected status").isEqualTo(400);
+    }
+
+    @Test
+    void nonExistingIdShouldReturnException() throws CustomBadRequestException, CustomNotFoundException {
+
+        userDTO.setUserId(2L);
+
+        HttpEntity<UserDTO> request = new HttpEntity<UserDTO>(userDTO, headers);
+
+        ResponseEntity<Object> actual = restTemplate.exchange(createURLWithPort("/users/2"), HttpMethod.PUT, request,
+        Object.class);
+
+        then(actual).as("Check if we have a response").isNotNull();
+        then(actual.getStatusCodeValue()).as("Check if we have the expected status").isEqualTo(404);
+    }
+
+    @Test
+    void shouldDeleteUserSuccessFully() throws CustomNotFoundException {
+
+        userDTO.setUserId(null);
+
+        HttpEntity<UserDTO> addRequest = new HttpEntity<UserDTO>(userDTO, headers);
+
+        ResponseEntity<Object> addResponse = restTemplate.exchange(createURLWithPort("/users"), HttpMethod.POST, addRequest,
+        Object.class);
+
+        then(addResponse).as("Check if we have a response").isNotNull();
+        then(addResponse.getStatusCodeValue()).as("Check if we have the expected status").isEqualTo(201);
+
+        HttpEntity<String> request = new HttpEntity<String>(null, headers);
+
+        ResponseEntity<Object> actual = restTemplate.exchange(createURLWithPort("/users/2"), HttpMethod.DELETE, request,
+        Object.class);
+
+        then(actual).as("Check if we have a response").isNotNull();
+        then(actual.getStatusCodeValue()).as("Check if we have the expected status").isEqualTo(200);
+    }
+
+    @Test
+    void nonExistingIdToDeleteShouldReturnException() throws CustomNotFoundException{
+
+        HttpEntity<String> request = new HttpEntity<String>(null, headers);
+
+        ResponseEntity<Object> actual = restTemplate.exchange(createURLWithPort("/users/4"), HttpMethod.DELETE, request,
+        Object.class);
+
+        then(actual).as("Check if we have a response").isNotNull();
+        then(actual.getStatusCodeValue()).as("Check if we have the expected status").isEqualTo(404);
+    }
+
+    private static String obtainAccessToken() throws Exception {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
@@ -83,6 +223,10 @@ public class UserControllerIT {
 
         JacksonJsonParser jsonParser = new JacksonJsonParser();
         return jsonParser.parseMap(result).get("access_token").toString();
+    }
+
+    private String createURLWithPort(String url) {
+        return "http://localhost:" + port + url;
     }
 
 }
